6d5562cd26816a82cf6c826188dc0b7365c7cacc
Maxim Grigorev <49513967+ey-ka@users.noreply.github.com>
2019-04-23 11:02:26 +0300

    Update Генератор паролей.py

--- Генератор паролей.py
+++ Генератор паролей.py
@@ -1,1 +1,6 @@
-Генератор паролей
+import random
+с = "abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZабвгдеёжзиклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"
+длина_пароля = 10
+п = "".join(random.sample(с, длина_пароля))
+print(п)
+input()
